9e4f146121bc0081875c1a33997442b0d789c6d5
lattalab <91266449+lattalab@users.noreply.github.com>
2022-02-08 22:13:13 +0800

    Add files via upload

---
+++ my_math.h
@@ -0,0 +1,9 @@
+#include <math.h>
+#define PI acos(-1)
+#define circle(x) PI*x*x
+#define rectangle(x,y) x*y
+#define triangle(base,height) 0.5*base*height
+#define SQUARE(x) x*x
+#define CUBIC(x) x*x*x
+#define AVERAGE(x,y) (x+y)/2.0
+#define PRODUCT(x,y) x*y
